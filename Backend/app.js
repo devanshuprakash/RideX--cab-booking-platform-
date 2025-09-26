ffd08d1eac2be1479c61a7c9fe165780b1bd16d9
Devanshu Prakash <devanshuprakash2301@gmail.com>
2025-09-26 12:16:46 +0530

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,18 @@
+const dotenv= require('dotenv');
+dotenv.config()
 const express = require('express');
+const cors = require('cors')
 const app = express();
+
+app.use(cors());
+
+
+
+
+
+
+
 app.get('/',(req,res)=>{
     res.send("hello");
 })
-module.exports=app;
+module.exports=app; 
