8ca3a5104b9b84712d2b44d856bcec4aeb7b035e
Devanshu Prakash <devanshuprakash2301@gmail.com>
2025-09-27 14:24:42 +0530

    home page designed

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const {body} = require('express-validator');
+
+router.post('/register', [
+    body('fullname.firstname').notEmpty().withMessage('First name is required'),
+    body('fullname.lastname').notEmpty().withMessage('Last name is required'),
+    body('email').isEmail().withMessage('Invalid email address'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], userController.registerUser);
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email address'),
+    body('password').notEmpty().withMessage('Password is required')
+], userController.loginUser);
+
+module.exports = router;
+
+
+
+
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -3,15 +3,29 @@ import React from 'react'
 const UserLogin = () => {
   return (
     <div className='p-7'>
+      <div>
+        <img  className="w-16  mb-10 " src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
       <form >
-        <h3 className='text-xl mb-2'>What's your email</h3>
-        <input type="email" required placeholder='abc@gmail.com' />
-        <h3>Enter Password</h3>
-        <input type="password" required placeholder='password' />
-        <button>Login</button>
+        <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+        <input type="email" required 
+        placeholder='abc@gmail.com'
+        className="bg-[#eeeeee] rounded mb-7
+        px-4 py-2 w-full text-lg border placeholder:text-base" />
+
+        <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+        <input type="password" required placeholder='password' className="bg-[#eeeeee] rounded mb-7
+        px-4 py-2 w-full text-lg border placeholder:text-base" />
+
+        <button className="bg-[#111] text-white font-semibold  rounded mb-7
+        px-4 py-2 w-full border text-base ">
+          Login</button>
       </form>
+      </div>
+      <div>
+         
+      </div>
     </div>
-  )
+  )  
 }
 
-export default UserLogin
+export default UserLogin;  
